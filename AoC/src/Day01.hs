069f59ac08324a31d40cb0f884e490f1a0e9fe2e
Travis Walton <2472981+waltont8@users.noreply.github.com>
2024-12-01 06:15:23 +0000

    Day 01 - Historian Hysteria

--- AoC/src/Day01.hs
+++ AoC/src/Day01.hs
@@ -3,20 +3,27 @@ module Day01
     ) where
 
 import Lazy
+import qualified Data.Map as M
 
 
 day01 :: String -> (String, String)
-day01 = lines >>> head >>> parse >>> part1 &&& part2 >>> tidy
+day01 = lines >>> parse >>> part1 &&& part2 >>> tidy
 
-parse :: String -> [String]
-parse = splitOn ","
+parse :: [String] -> [[Int]]
+parse p = transpose $ map (splitOn " " >>> filter (/= "") >>> map read ) p
 
-part1 :: [String] -> Int
-part1 s = sum $ map read s
+part1 :: [[Int]]-> Int
+part1 s = sum $ (\[a,b] -> zipWith (dist) b a) $ map sort s
 
 
-part2 :: [String] -> Int
-part2 s = sum $ map read s
+part2 :: [[Int]] -> Int
+part2 s = sum $ map (\x -> x * ( if M.member x right then right M.! x else 0)) left
+    where
+        inOrder = map sort s
+        left = inOrder!!0
+        right = M.fromList $ zip (map head $ group $ sort $ inOrder!!1) (map length $ group $ sort $ inOrder!!1)
 
 tidy :: (Int, Int) -> (String, String)
 tidy (a,b) = (show a, show b)
+
+dist a b = abs $ a - b
