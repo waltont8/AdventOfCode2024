2eb7afe7c9962210a445c95144fce192806b29cc
Trav <2472981+waltont8@users.noreply.github.com>
2024-12-03 08:14:47 +0000

    --- Day 3: Mull It Over ---

--- AoC/src/Day03.hs
+++ AoC/src/Day03.hs
@@ -2,21 +2,44 @@ module Day03
     ( day03
     ) where
 
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import Control.Monad.State
+import Data.Either
+import Data.Void
 import Lazy
 
+type Parser = Parsec Void String
+
+
+type Mul = (Int, Int)
+
+mulParser :: Parser Mul
+mulParser = do
+    void (string "mul(")
+    l <- L.decimal
+    void (string ",")
+    r <- L.decimal
+    void (string ")")
+    return (l,r)
 
 day03 :: String -> (String, String)
-day03 = lines >>> head >>> parse >>> part1 &&& part2 >>> tidy
+day03 s = (show $ doMuls s, show $ doMuls2 True s)
+    where
+        (l,r) = p s
 
-parse :: String -> [String]
-parse = splitOn ","
+doMuls s@(h:xs) = (l*r) + doMuls xs
+    where
+        (l,r) = p s
+doMuls [] = 0
 
-part1 :: [String] -> Int
-part1 s = sum $ map read s
 
+doMuls2 _ ('d':'o':'(':')':xs) = doMuls2 True xs
+doMuls2 _ ('d':'o':'n':'\'':'t':'(':')':xs) = doMuls2 False xs
+doMuls2 b s@(h:xs) = (if b then l*r else 0) + doMuls2 b xs
+    where
+        (l,r) = p s
+doMuls2 _ [] = 0
 
-part2 :: [String] -> Int
-part2 s = sum $ map read s
-
-tidy :: (Int, Int) -> (String, String)
-tidy (a,b) = (show a, show b)
+p s = fromRight (0,0) $ runParser mulParser "" s
