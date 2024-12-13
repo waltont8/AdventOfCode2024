24c2140911c7788be6b19caf5a0546ff68f89cc5
Travis Walton <2472981+waltont8@users.noreply.github.com>
2024-12-13 17:01:23 +0000

    --- Day 13: Claw Contraption ---

--- AoC/src/Day13.hs
+++ AoC/src/Day13.hs
@@ -4,19 +4,77 @@ module Day13
 
 import Lazy
 
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
+
+import Data.List
+import Data.Either
+import Data.Void
+type Parser = Parsec Void String
+
+data Machine = Machine
+  { ax :: Int
+  ,  ay :: Int
+  ,  bx :: Int
+  ,  by :: Int
+  ,  px :: Int
+  ,  py :: Int
+  } deriving Show
+
+-- Parse
+parseMachine :: Parser Machine
+parseMachine = do
+    ax <- string "Button A: X+" *> decimal
+    ay <- string ", Y+" *> decimal <* space1
+    bx <- string "Button B: X+" *> decimal
+    by <- string ", Y+" *> decimal <* space1
+    px <- string "Prize: X=" *> decimal
+    py <- string ", Y=" *> decimal
+    return Machine
+      {   ax = ax
+      ,   ay = ay
+      ,   bx = bx
+      ,   by = by
+      ,   px = px
+      ,   py = py
+      }
+
+parseMachines :: Parser [Machine]
+parseMachines = sepEndBy1 parseMachine space1
 
 day13 :: String -> (String, String)
-day13 = lines >>> head >>> parse >>> part1 &&& part2 >>> tidy
+day13 = parse' >>> part1 &&& part2 >>> tidy
 
-parse :: String -> [String]
-parse = splitOn ","
+parse' :: String -> [Machine]
+parse' s = case parse parseMachines "" s of
+         Left bundle -> trace (errorBundlePretty bundle) $ []
+         Right xs -> xs
 
-part1 :: [String] -> Int
-part1 s = sum $ map read s
+part1 :: [Machine] -> Int
+part1 m = foldl (\t (a,b) -> t+(a*3+b*1)) 0 wins
+    where
+        wins = map (canWin 0) m
 
 
-part2 :: [String] -> Int
-part2 s = sum $ map read s
+part2 :: [Machine] -> Int
+part2 m = foldl (\t (a,b) -> t+(a*3+b*1)) 0 wins
+    where
+        wins = map (canWin 10000000000000) m 
 
 tidy :: (Int, Int) -> (String, String)
 tidy (a,b) = (show a, show b)
+
+canWin :: Int -> Machine -> (Int,Int)
+canWin p (Machine ax ay bx by px py) = if af == fromIntegral ai && bf == fromIntegral bi then (ai, bi) else (0,0)
+    where
+        axf = (fromIntegral ax) :: Double
+        ayf = (fromIntegral ay) :: Double
+        bxf = (fromIntegral bx) :: Double
+        byf = (fromIntegral by) :: Double
+        pxf = (fromIntegral (px + p)) :: Double
+        pyf = (fromIntegral (py + p)) :: Double
+        af = (byf * pxf - bxf * pyf) / (byf * axf - bxf * ayf)  :: Double
+        bf = (pxf - axf * af) / bxf
+        ai = round af :: Int
+        bi = round bf :: Int
---
+++ AoC/src/day12.cpp
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define COLS 8
+#define ROWS 8
+
+char map[ROWS][COLS];
+
+void dump()
+{
+
+        for (int y=0;y<ROWS;y++)
+    {
+    for(int x=0;x<COLS;x++)
+        {
+            printf("%c", map[y][x]);
+        }
+            printf("\n");
+    }
+    printf("\n");
+}
+
+char get(int x, int y)
+{
+    if (x<0 || y<0 || x>=COLS || y >= ROWS) return '.';
+    return map[y][x];
+}
+
+int no(int x, int y, char target)
+{
+    if (get(x,y) != target && get(x,y) != (target - 32) && get(x,y) !='*')
+        return 1;
+
+    return 0;
+}
+
+void count2(char target, int x, int y, int *area, int *corner)
+{
+    if (get(x,y) != target) return;
+    map[y][x] = target - 32;
+
+    (*area)++;
+    if (no(x+1,y,target))
+    {
+        if (no(x,y+1,target)&&no(x+1,y+1,target))
+        {
+            (*corner)++;map[y][x]='*';
+        }
+        if (!no(x,y+1,target)&&!no(x+1,y+1,target))
+        {
+            (*corner)++;map[y][x]='*';
+        }
+    }
+    if (no(x-1,y,target))
+    {
+        if (no(x,y-1,target)&&no(x-1,y-1,target))
+        {
+            (*corner)++;map[y][x]='*';
+
+        }
+        if (!no(x,y-1,target)&&!no(x-1,y-1,target))
+        {
+            (*corner)++;   map[y][x]='*';         
+        }
+    }
+    if (no(x,y+1,target))
+    {
+        if (no(x-1,y,target)&&no(x-1,y+1,target))
+        {
+            (*corner)++;    map[y][x]='*';        
+
+        }
+        if (!no(x-1,y,target)&&!no(x-1,y+1,target))
+        {
+            (*corner)++;   map[y][x]='*';         
+
+        }        
+    }
+    if (no(x,y-1,target))
+    {
+        if (no(x+1,y,target)&&no(x+1,y-1,target))
+        {
+            (*corner)++;map[y][x]='*';
+        }
+        if (!no(x+1,y,target)&&!no(x+1,y-1,target))
+        {
+            (*corner)++;map[y][x]='*';
+        }    
+    }
+
+    count2(target, x+1, y, area, corner);
+    count2(target, x-1, y, area, corner);
+    count2(target, x, y+1, area, corner);
+    count2(target, x, y-1, area, corner);
+}
+
+void part2()
+{
+    int total = 0;
+    for(int x=0;x<COLS;x++)
+    {
+        for (int y=0;y<ROWS;y++)
+        {
+            if (map[y][x] >= 'a' && map[y][x] <= 'z')
+            {
+                int area = 0;
+                int corner = 0;
+                char target = map[y][x];
+                count2(map[y][x],x,y,&area,&corner);
+                total += area*corner;
+            }
+        }
+    }
+    printf("Part2: %d\n", total);
+}
+
+void count(char target, int x, int y, int *area, int *perim)
+{
+    if (get(x,y) != target) return;
+    map[y][x] = target + 32;
+
+    (*area)++;
+    if (get(x+1,y) != target && get(x+1,y) != (target + 32)) (*perim)++;
+    if (get(x-1,y) != target && get(x-1,y) != (target + 32)) (*perim)++;
+    if (get(x,y+1) != target && get(x,y+1) != (target + 32)) (*perim)++;
+    if (get(x,y-1) != target && get(x,y-1) != (target + 32)) (*perim)++;
+
+    count(target, x+1, y, area, perim);
+    count(target, x-1, y, area, perim);
+    count(target, x, y+1, area, perim);
+    count(target, x, y-1, area, perim);
+}
+
+void part1()
+{
+    int total = 0;
+    for(int x=0;x<COLS;x++)
+    {
+        for (int y=0;y<ROWS;y++)
+        {
+            if (map[y][x] >= 'A' && map[y][x] <= 'Z')
+            {
+                int area = 0;
+                int perim = 0;
+                char target = map[y][x];
+                count(map[y][x],x,y,&area,&perim);
+                total += area*perim;
+            }
+        }
+    }
+    printf("Part1: %d\n", total);
+}
+
+
+
+int main(void)
+{
+  char *input_line = NULL;
+  size_t lim = 0;
+
+  int line_count = 0;
+
+  while(getline(&input_line, &lim, stdin) > 1){
+    strcpy(map[line_count], input_line);
+    ++line_count;
+  }
+
+   part1();
+   dump();
+   part2();
+   dump();
+}
